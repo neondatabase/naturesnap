415ced8db60ae270e50dcde76ca37317b38af4f0
Raouf Chebri <raouf@chebri.com>
2023-02-03 12:23:46 +0100

    remove migrations

--- prisma/migrations/20230127150107_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Nature" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "seen" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "Nature_pkey" PRIMARY KEY ("id")
-);
