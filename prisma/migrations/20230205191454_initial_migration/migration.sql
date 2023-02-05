0fbdf01917cd541a33e607f6f2110f9e3e42f2ff
Anna Stepanyan <anna.stepanyan@zenith.tech>
2023-02-06 00:37:07 +0100

    update the initial migration

--- prisma/migrations/20230205191454_initial_migration/migration.sql
+++ prisma/migrations/20230205191454_initial_migration/migration.sql
@@ -35,3 +35,48 @@ ALTER TABLE "snaps" ADD CONSTRAINT "snaps_authorId_fkey" FOREIGN KEY ("authorId"
 
 -- AddForeignKey
 ALTER TABLE "snaps" ADD CONSTRAINT "snaps_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "topics"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+
+-- INSERT USERS DATA
+
+INSERT INTO users ("username", "avatar") VALUES ('anna@example.com', 'me.png');
+INSERT INTO users ("username", "avatar") VALUES ('joe@example.com', 'user1.jpeg');
+INSERT INTO users ("username", "avatar") VALUES ('alice@example.com', 'user2.jpeg');
+INSERT INTO users ("username", "avatar") VALUES ('bob@example.com', 'user3.jpeg');
+INSERT INTO users ("username", "avatar") VALUES ('jane@example.com', 'user4.jpeg');
+INSERT INTO users ("username", "avatar") VALUES ('dave@example.com', 'user5.jpeg');
+
+-- INSERT TOPICS DATA
+
+INSERT INTO topics ("name") VALUES ('islands');
+INSERT INTO topics ("name") VALUES ('night sky');
+INSERT INTO topics ("name") VALUES ('wolfs');
+INSERT INTO topics ("name") VALUES ('boats');
+INSERT INTO topics ("name") VALUES ('monkeys');
+INSERT INTO topics ("name") VALUES ('glaciers');
+INSERT INTO topics ("name") VALUES ('flowers');
+INSERT INTO topics ("name") VALUES ('beach');
+INSERT INTO topics ("name") VALUES ('birds');
+INSERT INTO topics ("name") VALUES ('lakes');
+INSERT INTO topics ("name") VALUES ('cats');
+INSERT INTO topics ("name") VALUES ('elephants');
+
+-- INSERT SNAPS DATA
+
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Raja Ampat', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp', '1', '1', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Milky Way', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp', '2', '2', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Boat', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp', '3', '4', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Wolf', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp', '4', '3', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Glacier', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp', '5', '6', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Flower', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp', '1', '7', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Monkeys', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(84).webp', '2', '5', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Camping', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(85).webp', '3', '8', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Cat', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(87).webp', '4', '11', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Flower', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(82).webp', '5', '7', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Flower', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(83).webp', '1', '7', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Lake', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(79).webp', '2', '10', '2023-01-31T14:08:11.404');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Raven', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(82).webp', '3', '9', '2023-01-31T22:28:59.223');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Owl', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(89).webp', '4', '9', '2023-01-31T22:28:59.223');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Kitty', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(90).webp', '5', '11', '2023-01-31T22:34:27.012');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('Paradise', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(91).webp', '3', '8', '2023-01-31T22:34:27.012');
+INSERT INTO snaps ("name", "image", "authorId", "topicId", "createdAt") VALUES ('elephant', 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(97).webp', '4', '12', '2023-01-31T22:36:30.283');
